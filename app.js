2027a586bc7367b8cf1e8c472db625ac1e3018d2
lior <lior.eshkhar@gmail.com>
2022-10-19 20:09:44 +0200

    Simple bar chart with dummy data

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const DUMMY_DATA = [
     { id: 'd4', value: 3, region: 'GERMANY' }
 ];
 
-d3.select('div')
-    .selectAll('p')
-    .data(DUMMY_DATA)
-    .enter()
-    .append('p')
-    .text(dta => dta.region);
+const container = d3.select('div')
+    .classed('container', true)
+    .style('border', '2px solid red');
+
+const bars = container
+    .selectAll('.bar')    // Selecting all the bar elements
+    .data(DUMMY_DATA)     // Bind data
+    .enter()              // Find what data was not rendered yet
+    .append('div')        // Add a div for every non rendered data point
+    .classed('bar', true) // Define all the created divs to .bar, matching the selection
+    .style('width', '50px')
+    .style('height', data => (data.value * 15) + 'px');
