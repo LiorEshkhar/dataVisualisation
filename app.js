7f366953b7c2d89829464677f51171c059cd0951
lior <lior.eshkhar@gmail.com>
2022-10-20 18:53:03 +0200

    Made the charat

--- app.js
+++ app.js
@@ -1,37 +1,62 @@
 const DUMMY_DATA = [
-    { id: 'd1', value: 1, region: 'USA' },
+    { id: 'd1', value: 2, region: 'USA' },
     { id: 'd2', value: 11, region: 'ISRAEL' },
-    { id: 'd3', value: 7, region: 'CYPRUS' },
+    { id: 'd3', value: 6, region: 'CYPRUS' },
     { id: 'd4', value: 8, region: 'GERMANY' }
 ];
 
-const xScale = d3.scaleBand()  // uniform scaling
-    // setting the different country names as the domain
-    .domain(DUMMY_DATA.map(dataPoint => dataPoint.region)) 
-    .rangeRound([0, 250])   // setting the possible range of values
-    .padding(0.2);
-const yScale = d3.scaleLinear() // scale linearaly to some values
-                .domain([0, 15]) // set the domain in some units
-                .range([0, 200]); // set the corresponding domain in pixels
+// Chart dimensions
+const MARGINS = {top: 20, bottom: 10};
+const CHART_WIDTH = 600;
+const CHART_HEIGHT = 400 - MARGINS.top - MARGINS.bottom;
 
-const container = d3.select('svg')
-    .classed('container', true)
+// Functions to set width and height
+const x = d3.scaleBand()          // scale uniformly
+    .rangeRound([0, CHART_WIDTH]) // set the range and round the values 
+    .padding(0.1);                // set the padding to 10%
+const y = d3.scaleLinear()        // scale linearly to the values
+.range([CHART_HEIGHT, 0]);        // set the range and keep the exact values
 
-const bars = container
-    .selectAll('.bar')    // Selecting all the bar elements
-    .data(DUMMY_DATA)     // Bind data
-    .enter()              // Find what data was not rendered yet
-    .append('rect')        // Add a div for every non rendered data point
-    .classed('bar', true) // Define all the created divs to .bar, matching the selection
-    .attr('width', xScale.bandwidth()) // set the width to be the same for all
-    .attr('height', (data) => yScale(data.value))  // set the height to depend on value
-    // the location of each country was defined in xScale (as region was the domain )
-    .attr('x', data => xScale(data.region))
-    .attr('y', data => 200 - yScale(data.value));
+// set the domain to be the regions, meaning the values will be attached to the regions
+x.domain(DUMMY_DATA.map(d => d.region)); 
+// set the domain to be a bit more than the full range of value
+y.domain([0, d3.max(DUMMY_DATA, d => d.value) + 3]);
 
-setTimeout(() => {
-    // Render the bars again only with the first 2 data pieces
-    bars.data(DUMMY_DATA.slice(0, 2))  
-    .exit()    // Returns the elements that were rendered, but whose data was removed
-    .remove()
-}, 3000);
+// select the svg and set its size
+const chartContainer = d3
+    .select('svg')
+    .attr('width', CHART_WIDTH)
+    .attr('height', CHART_HEIGHT + MARGINS.top + MARGINS.bottom);
+
+
+const chart = chartContainer.append('g'); // append g, an element used to group
+
+// Axis
+chart.append('g').call(d3.axisBottom(x).tickSizeOuter(0))
+.attr('color', '#666666')
+.attr('transform', `translate(0, 3)`)
+.attr('transform', `translate(0, ${CHART_HEIGHT})`);
+
+// Add the bars
+chart.selectAll('.bar')     // select all elements of class .bar (none yet)
+    .data(DUMMY_DATA)       // bind the data
+    .enter()                // select the missing elements
+    .append('rect')         // create a rect for each missing element
+    .classed('bar', true)   // set these rects to be of class bar
+    .attr('width', x.bandwidth())   // set the rects to be of uniform width
+    .attr('height', data => CHART_HEIGHT - y(data.value)) // linearly set the height
+    .attr('x', data => x(data.region))      // set the x coordinate
+    .attr('y', data => y(data.value));      // set the y coordinate
+
+// Add the labels
+chart.selectAll('.label')       // Select all labels (none yet)
+    .data(DUMMY_DATA)           // bind the data
+    .enter()                    // select the missing elements
+    .append('text')             // append the text element, not the word text
+    .text(data => data.value)   // set the text to be the value of each one
+    // set them to be in the middle of each bar
+    .attr('x', data => x(data.region) + x.bandwidth() / 2)  
+    .attr('y', data => y(data.value) - 20) // set them to be above each bar
+    .attr('text-anchor', 'middle') // the position of text will be decided by its middle
+    .classed('label', true)        // add them to class label
+
