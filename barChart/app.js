aaabbe2748682d4e8fd0751a2a5dcec1e88bb3f5
lior <lior.eshkhar@gmail.com>
2022-10-23 15:11:24 +0200

    Fixed barChart

--- barChart/app.js
+++ barChart/app.js
@@ -1,209 +1,130 @@
-let sample;
-let data = [];
-
-// declaring the functions so that they can be accessed by buttons,
-// allowing the user to scroll through the graph
-function next() { }
-function previous() { }
+const DUMMY_DATA = [
+    { id: 'd1', value: 2, region: 'USA' },
+    { id: 'd2', value: 11, region: 'ISRAEL' },
+    { id: 'd3', value: 6, region: 'CYPRUS' },
+    { id: 'd4', value: 8, region: 'GERMANY' },
+    { id: 'd5', value: 3.60, region: 'UK' }
+];
 
 // Chart dimensions
-let dim = {
-    'width': window.innerWidth * 0.7,
-    'height': window.innerHeight * 0.8,
-    'margin': window.innerHeight * 0.15
+const MARGINS = { top: 20, bottom: 10 };
+const CHART_WIDTH = window.innerWidth * 0.5;
+const CHART_HEIGHT = window.innerHeight * 0.8 - MARGINS.top - MARGINS.bottom;
+
+let selectedData = DUMMY_DATA;
+
+// select the svg and set its size
+const chartContainer = d3
+    .select('svg')
+    .attr('width', CHART_WIDTH)
+    .attr('height', CHART_HEIGHT + MARGINS.top + MARGINS.bottom);
+
+// Functions to set width and height
+const x = d3.scaleBand()          // scale uniformly
+    .rangeRound([0, CHART_WIDTH]) // set the range and round the values 
+    .padding(0.1);                // set the padding to 10%
+const y = d3.scaleLinear()        // scale linearly to the values
+    .range([CHART_HEIGHT, 0]);        // set the range and keep the exact values
+
+const chart = chartContainer.select('g'); // a svg element used to group elements
+
+function renderChart() {
+
+    // set the domain to be the regions, meaning the values will be attached to the regions
+    x.domain(selectedData.map(d => d.region));
+    // set the domain to be a bit more than the full range of value
+    y.domain([0, d3.max(selectedData, d => d.value) + 3]);
+
+    // Determine color according to value (defining a function here)
+    let color = d3.scaleSequential()
+        .domain([0, d3.max(selectedData, d => d.value)])
+        .interpolator(d3.interpolateRgb("#D9F9A5", "#1B98E0"));
+
+    // Axis
+    chart.select('#axis')
+        .transition()
+        // Add an axis according to the information stored in x (domain, location)
+        .call(d3.axisBottom(x).tickSizeOuter(0)) // bottom - labels are below the axis
+        .attr('color', '#666666')
+        // move the axis 0 in the x direction and height in the y direction
+        .attr('transform', `translate(0, ${CHART_HEIGHT})`);
+
+    // Update the bars
+    chart.selectAll('.bar')     // select all elements of class .bar (none yet)
+        .data(selectedData, data => data.id)       // bind the data
+        .join(enter => enter
+            .append('rect')
+            .classed('bar', true)   // set these rects to be of class bar
+            .attr('width', x.bandwidth())   // set the rects to be of uniform width
+            .attr('height', 0)
+            .attr('y', CHART_HEIGHT)
+            .attr('x', data => x(data.region))      // set the x coordinate
+            .transition().duration(600)             // add an animation
+            //linearly set the height
+            .attr('y', data => y(data.value))       // set the y coordinate
+            .attr('height', data => CHART_HEIGHT - y(data.value)),
+
+            update => update
+            .transition().duration(600)             // add an animation
+            .attr('x', data => x(data.region))      // set the x coordinate
+            .attr('width', x.bandwidth()),   // set the rects to be of uniform width
+
+            exit => exit
+            .transition().duration(300)
+            //linearly set the height
+            .attr('y', CHART_HEIGHT)
+            .attr('width', 0)
+            .attr('transform', `translate(${x.bandwidth()/2}, 0)`)
+            .attr('height', 0)
+            .remove()
+            )
+        .attr('fill', data => color(data.value))
+
+
+    // Update the labels
+    chart.selectAll('.label')       // Select all labels (none yet)
+        .data(selectedData, data => data.id)           // bind the data
+        .join('text')             // remove and append the needed labels
+        .text(data => data.value)   // set the text to be the value of each one
+        // set them to be in the middle of each bar
+        .attr('x', data => x(data.region) + x.bandwidth() / 2)
+        .attr('y', CHART_HEIGHT - CHART_HEIGHT * 0.1)
+        .attr('text-anchor', 'middle') // the position of text will be decided by its middle
+        .classed('label', true)        // add them to class label
+        .attr('opacity', -5)            // labels initially invisible (negative so that they appear later)
+        .attr('fill', data => color(data.value))
+        .transition().duration(800)
+        .attr('y', data => y(data.value) - CHART_HEIGHT * 0.05) // set them to be above each bar
+        .attr('opacity', 1);           // turn labels to visible 
 }
 
-// Craete the chart
-let svg = d3.select('#chart1')
-    .append('svg')
-    .attrs(dim);
-// container for the graph (path) itself
-let g = svg.append('g');
+renderChart();
 
-// convert the array read from the csv file to an array of objects
-function convToArray(sample) {
-    sample.forEach(element => {
-        data.push({
-            date: d3.timeParse("%Y-%m-%d")(element[0]),
-            price: parseFloat(element[1]) // convert string to float
-        })
-    })
-}
+// Selectors
+let unselectedIds = [];
 
-// async/await because the rest can only run once the data is rendered
-// fetch is asynchon by default, meaning the rest of the code dods not wait for it to finish
-async function renderData() {
-    const resp = await fetch('./data.csv');
-    sample = await resp.text();
-    // csv.toArrays converts each line to an array
-    // slice(1) removes the headers
-    sample = await $.csv.toArrays(sample).slice(1);
-    convToArray(sample);
-}
+// Add a list with number of elements equal to the number of countries
+const listItmes = d3.select('#data')
+    .select('ul')
+    .selectAll('li')
+    .data(DUMMY_DATA)
+    .enter()
+    .append('li');
 
-// asynb function used to make sure the data is rendered before the rest is executed
-(async function () {
-    await renderData();
+// Add the names of the countries
+listItmes.append('span')
+    .text(data => data.region);
 
-    // function to generate x axis
-    let scaleX = d3.scaleTime()
-        .nice()
-        .range([dim.margin, dim.width - dim.margin]);
-
-    // create the x axis
-    let axisX = svg.append('g')
-        .attr('transform', `translate(0, ${dim.height - dim.margin})`)
-        .attr('color', '#2b2d42');
-
-
-
-    // function to generate y axis
-    let scaleY = d3.scaleLinear()
-        .nice()
-        // +2 due to compensate for the width of the x axis and align the axes
-        .range([dim.height - dim.margin + 2, dim.margin]);
-
-    // create the y axis
-    let axisY = svg.append('g')
-        .attr('transform', `translate(${dim.margin}, 0)`)
-        .attr('color', '#2b2d42');
-
-    // line generator
-    let line = d3.line()
-        .x(d => scaleX(d.date))     // how x values should be calculated
-        .y(d => scaleY(d.price));   // how y values should be calculated
-
-    // area generator
-    let area = d3.area()
-        .x(d => scaleX(d.date))
-        .y0(d => scaleY(0))
-        .y1(d => scaleY(d.price));
-
-    // the duration each section will be viewed for
-    let cycle = 1500;
-
-    // define the section of the data to be viewed
-    let intervalEnd = 150;
-    let intervalIncrement = 150;
-
-    // view the next section
-    next = function () {
-        intervalEnd += intervalIncrement;
-
-        // if at the end
-        if (intervalEnd >= data.length)
-            // disable the "next" button
-            document.getElementById('nextBtn').disabled = true;
-        document.getElementById('previousBtn').disabled = false;
-
-        sendDraw();
-    }
-
-    // view the previous section
-    previous = function () {
-        intervalEnd -= intervalIncrement;
-
-        // if at the beginning
-        if (intervalEnd <= intervalIncrement)
-            // disable the "previous" button
-            document.getElementById('previousBtn').disabled = true;
-        document.getElementById('nextBtn').disabled = false;
-
-        sendDraw();
-    }
-
-    // automatically run through the sections
-    let recurringDraw = setInterval(next, cycle);
-
-    // send the current section of data to draw
-    function sendDraw() {
-        // if at the end, stop the automatic loop
-        if (intervalEnd > data.length)
-            clearInterval(recurringDraw);
-
-        draw(data.slice(Math.max(intervalEnd - intervalIncrement, 0), intervalEnd));
-    }
-
-    // draw and update the graph
-    function draw(data) {
-        let t = d3.transition().duration(500)
-
-        // update the domain of the generator for the x axis
-        scaleX.domain(d3.extent(data, d => d.date))
-
-        // update the x axis
-        axisX.transition(t)
-            .call(d3.axisBottom(scaleX)
-                .tickFormat(d3.timeFormat("%m-%Y"))
-                .tickSize(10)
-                .tickSizeOuter(0)
-                .ticks(8))
-            .selectAll('text');
-
-        // style the x axis
-        styleAxis(axisX);
-
-        // update the domain of the generator for the y axis
-        scaleY
-            .domain([0, d3.max(data, d => d.price)])
-
-        // update the y axis
-        axisY.transition(t)
-            .call(d3.axisLeft(scaleY)
-                .ticks(5)
-                .tickSizeOuter(0))
-
-        // style the y axis
-        styleAxis(axisY);
-
-        // update the graph
-        g.selectAll('path')
-            // putting data inside of an array so that there is one peace of data
-            // and the graph is drawn once, otherwise the graph would be drawn once
-            // for each element in the array
-            .data([data], d => d[0].date)
-
-            // draw the new path
-            .join(enter => enter
-                .append('path')
-                .attrs({
-                    'fill': 'none',
-                    'stroke': '#8d99ae',
-                    'stroke-width': '2px',
-                    'd': line(data)         // generator
-                }),
-
-                update => update,
-
-                // remove the old path
-                exit => exit
-                    .transition().duration(1000)
-                    .attr('d', line(data))
-                    .remove())
-    }
-
-    // style the axes
-    function styleAxis(axis) {
-        // the line
-        axis.selectAll('.domain')
-            .attrs({
-                'stroke': '#E04836',
-                'stroke-width': 6
-            });
-        // the labels
-        axis.selectAll('text')
-            .attrs({
-                'font-size': '1rem',
-                'color': '#381501'
-            })
-
-        // the ticks
-        axis.selectAll('line')
-            .attrs({
-                'stroke': '#E04836',
-                'stroke-width': 3,
-                'opacity': .8
-            });
-    }
-})();
-
+// Add checkboxes and manage the events of them being checked or unchecked
+listItmes.append('input')
+    .attr('type', 'checkbox')
+    .attr('checked', true)
+    .on('change', (e, data) => {
+        if (unselectedIds.indexOf(data.id) === -1) {
+            unselectedIds.push(data.id);
+        } else {
+            unselectedIds = unselectedIds.filter(id => id !== data.id);
+        }
+        selectedData = DUMMY_DATA.filter(d => unselectedIds.indexOf(d.id) === -1);
+        renderChart();
+    });
